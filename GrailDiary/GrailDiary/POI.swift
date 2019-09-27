951f02afa4da2101b242d7704fe0ecc6468c6550
CrystalKnightCodes <CrystalKnightCodes@gmail.com>
2019-09-27 16:05:28 -0400

    Added and organized View Controller, Custom View, and Model files.

---
+++ GrailDiary/GrailDiary/POI.swift
@@ -0,0 +1,9 @@
+//
+//  POI.swift
+//  GrailDiary
+//
+//  Created by Christy Hicks on 9/27/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import Foundation
---
+++ GrailDiary/GrailDiary/POITableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  POITableViewCell.swift
+//  GrailDiary
+//
+//  Created by Christy Hicks on 9/27/19.
+//  Copyright © 2019 Knight Night. All rights reserved.
+//
+
+import UIKit
+
+class POITableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
